92e72d6249c758a2bd3b55f3817a2d14a5a6e9fd
Daniel Cardoso <danielcardosods@gmail.com>
2018-11-26 20:34:49 +0200

    Add optional field to type Vehicle

--- src/app/asset-manager/asset.ts
+++ src/app/asset-manager/asset.ts
@@ -1,8 +1,30 @@
 import { AssetType } from './asset-type.enum';
 
+interface OptionalField {
+  key: string;
+  label: string;
+  value: string;
+}
+
 export class Asset {
-  id: number;
+  id?: number;
   name: String;
   type: AssetType;
   quantity: number;
+  optionalFields: OptionalField[] = [];
+
+  setType(type: AssetType) {
+    if (type === AssetType.Vehicle
+      && !this.optionalFields.length) {
+      this.optionalFields.push({
+        key: 'licensePlate',
+        label: 'License plate',
+        value: ''
+      });
+    } else {
+      this.optionalFields = [];
+    }
+
+    this.type = type;
+  }
 }
