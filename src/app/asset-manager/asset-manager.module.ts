af4cfb32e950f50722353794fc8efe7fe1eb1165
Daniel Cardoso <danielcardosods@gmail.com>
2018-11-27 17:09:21 +0200

    Sort assets by name

--- src/app/asset-manager/asset-manager.module.ts
+++ src/app/asset-manager/asset-manager.module.ts
@@ -8,9 +8,10 @@ import { AssetManagerComponent } from './asset-manager/asset-manager.component';
 import { AssetService } from './asset.service';
 import { AssetFormComponent } from './asset-form/asset-form.component';
 import { AssetFilterComponent } from './asset-filter/asset-filter.component';
+import { AssetSorterComponent } from './asset-sorter/asset-sorter.component';
 
 @NgModule({
-  declarations: [AssetManagerComponent, AssetFormComponent, AssetFilterComponent],
+  declarations: [AssetManagerComponent, AssetFormComponent, AssetFilterComponent, AssetSorterComponent],
   imports: [
     CommonModule,
     HttpClientModule,
---
+++ src/app/asset-manager/asset-sorter/asset-sorter.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-sorter',
+  templateUrl: './asset-sorter.component.html',
+  styleUrls: ['./asset-sorter.component.css']
+})
+export class AssetSorterComponent implements OnInit {
+
+  @Input() sortBy: string;
+  @Output() sorted = new EventEmitter<object>();
+
+  sortByLabel = '';
+  direction = '';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.sortByLabel = this.sortBy;
+    this.sortByLabel = this.sortByLabel.charAt(0).toUpperCase() + this.sortByLabel.slice(1);
+  }
+
+  toggleSort() {
+    const sorter = {};
+    if (this.direction === 'asc') {
+      this.direction = 'desc';
+    } else {
+      this.direction = 'asc';
+    }
+    sorter['_sort'] = this.sortBy;
+    sorter['_order'] = this.direction;
+    console.log(sorter);
+    this.sorted.emit(sorter);
+  }
+
+}
